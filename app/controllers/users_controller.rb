2e6ec2346b50107a666b161d4eb0cbb71caedee5
Bobby Matson <bobby.matson@gmail.com>
2013-03-17 03:55:19 -0700

    disable index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    # @users = User.all
 
     respond_to do |format|
       format.html # index.html.erb
