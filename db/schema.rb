485c5352020e6893edf9f0beae55959a164e1a02
Bobby Matson <bobby.matson@gmail.com>
2013-04-07 12:29:49 -0700

    anonymous boolean on user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319023403) do
+ActiveRecord::Schema.define(:version => 20130406223718) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "anonymous"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
