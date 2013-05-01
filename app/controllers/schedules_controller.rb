29daebf7ef4ef535b1f4f3ef1eb72ae0e75039b2
Bobby Matson <bobby.matson@gmail.com>
2013-04-30 22:09:57 -0700

    crud actions for schedule controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,24 +1,48 @@
 class SchedulesController < ApplicationController
+   before_filter :load_user
 
   def new
     # @user = User.find(params[:id])
-    @user = User.first
     @schedule = Schedule.new(:user_id => @user.id)
     @schedule.timeslots.build
   end
 
   def create
-    @schedule = Schedule.new(params[:schedule])
+    @schedule = @user.build_schedule(params[:schedule])
+#    @schedule = Schedule.new(params[:schedule])
     if @schedule.save
       flash[:notice] = "Schedule created."
       redirect_to schedule_path(@schedule)
     else
       flash[:error] = "You failed, give up."
-      redirect_to new_schedule_path
+      render :action => :new
     end
   end
 
   def show
-    @schedule = Schedule.find(params[:id])
+    # @schedule = Schedule.find(params[:id])
+    @schedule = @user.schedule
+  end
+
+  def edit
+    @schedule = @user.schedule
+  end
+
+  def update
+    @schedule = @user.schedule
+    @schedule.attributes = params[:schedule]
+    if @schedule.save
+      flash[:notice] = "Schedule updated."
+      redirect_to schedule_path(@schedule)
+    else
+      flash[:error] = "You failed, give up."
+      render :action => :edit
+    end
+  end
+
+
+  private
+  def load_user
+    @user = User.first
   end
 end
