e3200314e22eda1e121e60c504c095ddc0b9a8b6
Bobby Matson <bobby.matson@gmail.com>
2013-06-05 17:49:28 -0700

    finally a decent auth model, now logs in user once they are newly created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      if params[:remember_me]
-        cookies.permanent[:auth_token] = user.auth_token
-      else
-        cookies[:auth_token] = user.auth_token
-      end
+      sign_in user
       redirect_back root_url, notice: "Logged in!"
     else
       flash.now.alert = "Invalid, my friend! Try again"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,13 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
+
+  def sign_in(user)
+    if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+    else
+        cookies[:auth_token] = user.auth_token
+    end
+    current_user = user                      
+  end
 end
