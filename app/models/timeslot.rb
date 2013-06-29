095f626bc20b3a8255e65b0e0f372d1d2e6024ea
Bobby Matson <bobby.matson@gmail.com>
2013-06-29 16:43:36 -0700

    only show upcoming timeslots

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -6,14 +6,15 @@ class Timeslot < ActiveRecord::Base
   validates :start_time, presence: true
 
   scope :available, where("confirmed != ?", true)
-  scope :closest, available.where("start_time <= ?", DateTime.now)
+  scope :closest, available.where("start_time >= ?", DateTime.now)
+  scope :past, where("start_time < ?", DateTime.now)
 
   # after_initialize do
   #   self.confirmed  ||= false
   # end
 
   def self.group_by_date(scope = scoped)
-    scope.all.group_by {|i| i.start_time.to_date}
+    scope.closest.group_by {|i| i.start_time.to_date}
   end
 
   def formatted_start_time
@@ -23,5 +24,6 @@ class Timeslot < ActiveRecord::Base
   def only_start_time
     self.start_time.strftime("%I:%M %p")
   end
+
 
 end
